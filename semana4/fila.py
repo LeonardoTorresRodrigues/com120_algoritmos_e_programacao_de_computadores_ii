073c3008b58bf9eba320d4651d3ad48ef8186919
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2026-04-25 18:48:40 -0300

    feat: semana4: implementação de fila

---
+++ semana4/fila.py
@@ -0,0 +1,25 @@
+class Fila():
+    def __init__(self):
+        self.data = []
+
+    def inserir(self, x):
+        self.data.append(x)
+
+    def remover(self):
+        if len(self.data) > 0:
+            return self.data.pop(0)
+
+    def top(self):
+        if len(self.data) > 0:
+            return self.data[0]
+
+    def empty(self):
+        return not len(self.data) > 0
+
+
+f = Fila()
+f.inserir(1)
+f.inserir(2)
+f.inserir(3)
+f.inserir(4)
+f.remover()
